45b1e71cf0c6f7dcff3f8f6c74a7cc99dc7077fd
Egor <dementev.eg@yandex.ru>
2025-05-02 14:10:38 +0300

    first setup application

---
+++ src/test/java/com/example/report_server/ReportServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.report_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
